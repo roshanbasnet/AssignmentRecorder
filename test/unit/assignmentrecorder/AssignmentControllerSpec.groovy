7d961fa9819c4a87a999c9642128f0db7f949c02
roshanbasnet <roshanbasnet26@gmail.com>
2016-06-03 14:16:59 +0545

    controller

---
+++ test/unit/assignmentrecorder/AssignmentControllerSpec.groovy
@@ -0,0 +1,20 @@
+package assignmentrecorder
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(AssignmentController)
+class AssignmentControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
